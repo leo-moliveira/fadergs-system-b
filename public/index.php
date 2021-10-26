97b33e5cac2d7f67905674b1f94bae1ae5a56348
Leonardo Oliveira <leo.oliveira8@gmail.com>
2021-10-26 19:54:30 -0300

    fix Swagger assets

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
+    return false;
+}
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
